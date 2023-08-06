0a531548db3c5eb4b2a17a7d83856955f484c0c0
yashwanth938 <rongalayaswanth@gmail.com>
2023-08-06 22:04:16 +0530

    updated

--- routes/product-routs.js
+++ routes/product-routs.js
@@ -2,4 +2,13 @@ const express = require('express');
 const product=require("../Model/Product");
 const router = express.router();
 
+router.get("/",async(req,res,next)=> {
+    let product = await product.find();
 
+    if(!products){
+        return res.status(404).json({message:'No products'})
+    }
+    res.status(200).json({products})
+});
+
+module.exports = router;
