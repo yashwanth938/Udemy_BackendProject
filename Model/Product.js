27d1753d5cb310f43e4e36fc7d75a6eb29d06c49
yashwanth938 <rongalayaswanth@gmail.com>
2023-08-06 21:49:09 +0530

    write the schema

--- Model/Product.js
+++ Model/Product.js
@@ -1,4 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');\
 
+
+const Schema = mongoose = mongoose.Schema;
+
+const productSchema =new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    imgURL:{
+        type:String,
+        required:true,
+    },
+})
 
