588cbe689227b8080d1f9597c9957272e31fefeb
yashwanth938 <rongalayaswanth@gmail.com>
2023-08-06 21:00:54 +0530

    changes done

--- app.js
+++ app.js
@@ -8,18 +8,4 @@ app.get("/",(req,res)=>{
     res.send("Hello world");
 });
 
-//connection and PORT
-mongoose.connect(
-    "mongodb+srv://yashrongala:<password>@rongalas.cnexybn.mongodb.net/?retryWrites=true&w=majority"
-    {
-        
-        UseNewUrlParser:true,
-        useUnifiedTopology:true
-    }
-).then(()=>{
-    console.log("connected to database");
-}).catch(err=>{
-    console.log(err);
-});
 
-app.listen(5000);
